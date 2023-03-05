6a1b93e9d7263d3ec915db45d74a17d6d74820b1
Anarasty <murloc.ninja16@gmail.com>
2023-03-05 09:35:01 +0200

    login maybe done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 import BookList from "./components/BookList";
 import Cart from "./components/Cart";
 import LoginPage from "./components/LoginPage";
@@ -8,6 +8,13 @@ import Book from "./components/Book";
 import "./App.css";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem("user"));
+
+  function handleLogout() {
+    localStorage.removeItem("user");
+    setLoggedIn(false);
+  }
+
   return (
     <BrowserRouter>
       <div className="App">
@@ -19,18 +26,50 @@ function App() {
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/1024px-LEGO_logo.svg.png"
                 alt="My Bookstore"
               />
-              <Link to="/">Logo</Link>
+              <Link to="/booklist">Logo</Link>
             </div>
             <div className="nav-buttons">
               <Link to="/cart">Cart</Link>
-              <Link to="/login">Login</Link>
+              {/* <Link to="/">Login</Link> */}
+              {loggedIn && (
+                <div>
+                  Logged in as {localStorage.getItem("user")}.
+                  <button onClick={handleLogout}>Sign out</button>
+                </div>
+              )}
             </div>
           </nav>
         </header>
         <Routes>
-          <Route path="/" element={<BookList />} />
+          {/* <Route path="/booklist" element={<BookList />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/*" element={<NotFoundPage />} />
+          <Route path="book/:id" element={<Book />} /> */}
+          <Route
+            path="/"
+            element={
+              loggedIn ? <Navigate to="/booklist" /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/booklist"
+            element={loggedIn ? <BookList /> : <Navigate to="/login" replace />}
+          />
+          <Route
+            path="/cart"
+            element={loggedIn ? <Cart /> : <Navigate to="/login" replace />}
+          />
+          <Route
+            path="/login"
+            element={
+              loggedIn ? (
+                <Navigate to="/booklist" replace />
+              ) : (
+                <LoginPage onLogin={() => setLoggedIn(true)} />
+              )
+            }
+          />
           <Route path="/*" element={<NotFoundPage />} />
           <Route path="book/:id" element={<Book />} />
         </Routes>
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,33 +1,75 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./LoginPage.css";
 
-function LoginPage() {
+// function LoginPage() {
+//   const [username, setUsername] = useState("");
+
+//   const handleInputChange = (event) => {
+//     setUsername(event.target.value);
+//   };
+
+//   const handleLogin = () => {
+//     // Здесь можно добавить логику для обработки входа пользователя
+//     console.log(`Пользователь ${username} вошел в систему!`);
+//   };
+
+//   return (
+//     <div className="login-container">
+//       <img
+//         src="https://www.graphicsprings.com/filestorage/stencils/68ea7d075a2064907de0c873ea1d81f3.png?width=500&height=500"
+//         alt="Технологии"
+//       />
+//       <form>
+//         <label>
+//           Login:
+//           <input type="text" value={username} onChange={handleInputChange} />
+//         </label>
+//         <button type="button" onClick={handleLogin}>
+//           Войти
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
+function LoginPage({ onLogin }) {
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
+  const [usernameError, setUsernameError] = useState(false);
 
-  const handleInputChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handleLogin = () => {
-    // Здесь можно добавить логику для обработки входа пользователя
-    console.log(`Пользователь ${username} вошел в систему!`);
-  };
+  function handleLogin() {
+    if (username.length >= 4 && username.length <= 16) {
+      localStorage.setItem("user", username);
+      setUsernameError(false);
+      onLogin();
+      navigate("/booklist");
+    } else {
+      setUsernameError(true);
+    }
+  }
 
   return (
-    <div className="login-container">
-      <img
-        src="https://www.graphicsprings.com/filestorage/stencils/68ea7d075a2064907de0c873ea1d81f3.png?width=500&height=500"
-        alt="Технологии"
-      />
-      <form>
-        <label>
-          Login:
-          <input type="text" value={username} onChange={handleInputChange} />
-        </label>
-        <button type="button" onClick={handleLogin}>
-          Войти
-        </button>
-      </form>
+    <div>
+      <img src="logo.png" alt="Logo" />
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        {usernameError && (
+          <span style={{ color: "red" }}>
+            Username must be between 4 and 16 characters.
+          </span>
+        )}
+      </label>
+      <button
+        disabled={username.length < 4 || username.length > 16}
+        onClick={handleLogin}
+      >
+        Log in
+      </button>
     </div>
   );
 }
