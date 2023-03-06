ee1ec1d574a456ae96410925981868fe507a65b3
Anarasty <murloc.ninja16@gmail.com>
2023-03-06 22:13:02 +0200

    styles v3.0

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -5,6 +5,7 @@ function NotFoundPage() {
   return (
     <div className="notfound_page">
       <h1>404</h1>
+      <h3>Page not found</h3>
       <i className="fa-sharp fa-solid fa-circle-xmark"></i>
     </div>
   );
